0a9f0531abad9bf090d29fbec6d65d16a87e774f
HiggyMeow <Mc_MHiggins26@mcjvs2.org>
2025-10-08 08:28:40 -0400

    first commit

---
+++ js01/about-me.js
@@ -0,0 +1,13 @@
+//Gives a pop up for a welcome
+alert('Welcome to my about me!');
+//makes a header with my name
+document.write('<h1>Max</h1>');
+//log messages
+console.log('if your reading this here is some fun facts about me!');
+console.log('1. I love pizza');
+console.log('2. I play video games');
+console.log('My favorite fast food is five guys');
+//paragraphs about me
+document.write('<p>My name is Max and I was born in ireland and moved to the united states later in my life. I really like video games and I stream them alot to the public in hopes that maybe ill be able to do it for a living</p>');
+document.write('<p>I drive and infiniti g37x which i am very proud of. One of my favorite things ever is my car and I glorify it so much</p>');
+document.write('<p>Thats all about me! thank you for listening I hope that you enjoyed reading this!</p>')
---
+++ js01/script.js
@@ -0,0 +1,3 @@
+//Max
+console.log('External file loaded!');
+document.write('Today i learned alot. I am now able to use the basics of java script to make my websites better then they were before. I learned specifailly how to add text to the website, display pop ups, and put things in the console.');
