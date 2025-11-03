826d37a35becd0943732ac16009089cfbb60e878
HiggyMeow <Mc_MHiggins26@mcjvs2.org>
2025-11-03 08:05:42 -0500

    js04

---
+++ JavaScriptGameTutorial/script.js
@@ -0,0 +1,40 @@
+const canvas = document.getElementById('canvas1');
+const ctx = canvas.getContext('2d')
+canvas.width = 900;
+canvas.height = 600;
+
+//global variables
+const cellSize = 100;
+const cellGap = 3;
+const gameGid = [];
+
+//game board
+//projectiles
+const controlsBar = {
+    width: canvas.width,
+    height: cellSize,
+}
+class Cell {
+    constructor(x, y){
+        this.x = x;
+        this.y = y;
+        this.width = cellSize;
+        this.height = cellSize;
+
+    }
+    draw(){
+        ctx.strokeStyle = 'black';
+        ctx.strokeStyle(this.x, this.y, this.width, this.height);
+        s
+    }
+}
+//defenders
+//enemies
+//resources
+//utilities
+function animate(){
+    ctx.fillStyle = 'blue';
+    ctx.fillRect(0,0,controlsBar.width,controlsBar.height);
+    requestAnimationFrame(animate);
+}
+animate();
